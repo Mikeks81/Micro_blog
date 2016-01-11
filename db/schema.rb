1e94ea93ef7bcf95240d4adf1270ec223629c678
Michael <Mikeks81@gmail.com>
2016-01-10 21:47:55 -0500

    added join table-- not functional yet

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160104220115) do
+ActiveRecord::Schema.define(version: 20160111020913) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20160104220115) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
   end
 
   create_table "users", force: :cascade do |t|
